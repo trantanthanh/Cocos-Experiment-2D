7be51aef299a28a451802b08c2d0400955ca0a4d
Thanh Tran Tan <trantanthanh@gmail.com>
2024-07-26 01:21:16 +0700

    moving by keyboard

--- assets/Scripts/KeyBoardScript.ts
+++ assets/Scripts/KeyBoardScript.ts
@@ -1,18 +1,43 @@
-import { _decorator, Component, EventKeyboard, input, Input, KeyCode, Node } from 'cc';
+import { _decorator, Component, EventKeyboard, input, Input, Animation, KeyCode, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('KeyBoardScript')
 export class KeyBoardScript extends Component {
 
-    onLoad(){
+    @property moveSpeed: number = 100;
+    movingDir: number = 1;// 1: right, -1: left
+    isWalking: boolean = false;
+    onLoad() {
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
     }
 
-    onKeyDown(event : EventKeyboard){
+    onKeyDown(event: EventKeyboard) {
         switch (event.keyCode) {
-            case KeyCode.KEY_Z:
+            case KeyCode.ARROW_RIGHT:
                 {
-                    console.log("Z");
+                    this.isWalking = true;
+                    this.movingDir = 1;
+                    this.getComponent(Animation).crossFade('run');
+                    break;
+                }
+            case KeyCode.ARROW_LEFT:
+                {
+                    this.getComponent(Animation).crossFade('run');
+                    this.isWalking = true;
+                    this.movingDir = -1;
+                    break;
+                }
+        }
+    }
+
+    onKeyUp(event: EventKeyboard) {
+        switch (event.keyCode) {
+            case KeyCode.ARROW_RIGHT:
+            case KeyCode.ARROW_LEFT:
+                {
+                    this.getComponent(Animation).stop();
+                    this.isWalking = false;
                     break;
                 }
         }
@@ -23,7 +48,9 @@ export class KeyBoardScript extends Component {
     }
 
     update(deltaTime: number) {
-        
+        if (this.isWalking) {
+            this.node.setPosition(this.node.getPosition().x + this.movingDir * this.moveSpeed * deltaTime, this.node.getPosition().y);
+        }
     }
 }
 
